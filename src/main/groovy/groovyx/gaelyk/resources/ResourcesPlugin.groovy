bfb31c18a3e5cb3c642fd8f471b76c7877271ad6
Vladimir Orany <vladimir@orany.cz>
2013-02-22 19:29:45 +0100

    set first route index

--- src/main/groovy/groovyx/gaelyk/resources/ResourcesPlugin.groovy
+++ src/main/groovy/groovyx/gaelyk/resources/ResourcesPlugin.groovy
@@ -3,9 +3,15 @@ package groovyx.gaelyk.resources
 import groovyx.gaelyk.plugins.PluginBaseScript
 
 class ResourcesPlugin extends PluginBaseScript {
-
+    
+    /**
+     * This plugin is one of the core plugins, so the number is close to {@link Integer#MIN_VALUE}
+     */
+    static final int FIRST_ROUTE_INDEX = -60000
+    
     @Override
     public Object run() {
+        firstRouteIndex = FIRST_ROUTE_INDEX
         get "/gpr/@path", forward: "/groovyx/gaelyk/resources/server.groovy?&path=@path"
     }
 
